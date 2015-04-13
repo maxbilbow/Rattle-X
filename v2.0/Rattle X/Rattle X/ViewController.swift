ec0d7d144d269e602f0815d3c9fe1a8f9e271361
maxbilbow <maxbilbow@gmail.com>
2015-04-13 09:32:21 +0100

    Initial Commit

---
+++ v2.0/Rattle X/Rattle X/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rattle X
+//
+//  Created by Max Bilbow on 13/04/2015.
+//  Copyright (c) 2015 Rattle Media. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
